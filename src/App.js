fc132996e2110f596b5f762bbc0b5aa1c23b2af8
kusal97 <kusalsirimanna97@gmail.com>
2023-10-23 00:39:26 +0530

    Finish car list page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import MainScreen from "./screens/MainScreen";
 import "./styles/app.scss";
 import PreUsedCars from "./screens/PreUsedCars";
 import BrandNewCars from "./screens/BrandNewCars";
+import CarDetailsPage from "./screens/CarDetailsPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<MainScreen />} />
       <Route path="/pre-used" element={<PreUsedCars />} />
       <Route path="/brand-new" element={<BrandNewCars />} />
+      <Route path="/pre-used/details" element={<CarDetailsPage />} />
     </Routes>
   );
 }
--- src/screens/CarDetailsPage.js
+++ src/screens/CarDetailsPage.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { LeftOutlined } from "@ant-design/icons";
 
 const CarDetailsPage = () => {
-  return <div>CarDetailsPage</div>;
+  return (
+    <div className="details-page">
+      <div className="details-header">
+        <LeftOutlined className="back-icn" />
+        <div>
+          <div className="detail-title">HONDA CIVIC SR</div>
+          <div className="detail-year">2018/2019</div>
+        </div>
+      </div>
+      <div className="details-content">
+        <div className="details-gallery">gallery</div>
+        <div className="details-specification">Specifications</div>
+      </div>
+    </div>
+  );
 };
 
 export default CarDetailsPage;
